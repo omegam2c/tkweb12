cad87da79ee0a7ad33ed6ec0f0615142b6ff30bc
omegam2c <ducmcc123@gmail.com>
2025-06-03 15:21:10 +0700

    add fx

---
+++ slider.js
@@ -0,0 +1,37 @@
+let list= document.querySelector('.slider-container .list');
+let items = document.querySelectorAll('.slider-container .list .item');
+let dots = document.querySelectorAll('.slider-container .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems= items.length -1;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick= function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshSlider = setInterval(()=>{next.click()}, 4000);
+function reloadSlider(){
+    let checkLeft = items[active].offsetLeft;
+    list.style.left= -checkLeft +'px';
+    let lastActiveDot= document.querySelector('.slider-container .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(()=>{next.click()}, 4000);
+}
+
+dots.forEach((li,key)=>{
+    li.addEventListener('click',()=>{
+        active =key;
+        reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
